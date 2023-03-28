03e761af82306e4c8582bbbe0815c1b3f644654d
Isabelli Oliveira <104120168+IsabelliOliveira@users.noreply.github.com>
2023-03-28 13:45:31 -0300

    Commit

--- Código/Ex1.py
+++
@@ -1,9 +0,0 @@
-#Elaborar uma lgoritmo que ao receber a cotação do dólar e a quantidade de dólares para trocar por real deve exibir aquantidade em reais.
-
-Qtd=input("Digite o valor que deseja trocar \n")
-auxQtd=float(Qtd)
-R=auxQtd*5.25
-print("Voce deve receber %0.2f" %(R))
-#print("Voce deve receber ", (R))
-#print("Voce deve receber " + str(R))
-#print("Voce deve receber {}", format (R))
--- Código/Ex3.py
+++
@@ -1,5 +0,0 @@
-#Elaborar um algoritmo que leia um valor de temperatura em graus Celsius, calcule e exiba a temperatura equivalente em Kelvin, sabendo que: K = C + 273.
-
-C=float(input("Insira temperatura em Celsius \n"))
-K=C+273
-print("A temperatura em Kelvin é", K)
