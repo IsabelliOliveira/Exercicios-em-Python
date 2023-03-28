0f2461afb7ff95442fa6111a70f427ed9b6e3b53
Isabelli Oliveira <104120168+IsabelliOliveira@users.noreply.github.com>
2023-03-28 13:46:28 -0300

    Exercícios Basicos

---
+++ Basico/Código/Ex2.py
@@ -0,0 +1,5 @@
+#Elaborar um algoritmo que leia um comprimento em polegadas, calcule e exiba o comprimento em milímetros, considerando que 1 polegada equivale a 25,4 milímetros.
+
+Pol= float (input("Digite a medida em polegada \n"))
+mm= Pol*25.4
+print("O valor em polegada é", mm)
